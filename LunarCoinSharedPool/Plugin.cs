50bdb7079fbf7c75124f00d44d7c42bc56d24dbe
Kirefel <kirefelan@gmail.com>
2024-01-03 18:26:34 +1100

    Lunar coin shared pool

---
+++ LunarCoinSharedPool/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using R2API.Utils;
+using RoR2;
+using RoR2.Items;
+using System.Linq;
+
+namespace LunarCoinSharedPool;
+
+[BepInPlugin("kft.r2.lunarcoinsharedpool", "Lunar Coin Shared Pool", "1.0.0")]
+[BepInDependency("com.dan8991iel.LunarCoinShareOnPickup")]
+[BepInDependency("com.Varna.EphemeralCoins")]
+[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod)]
+public class Plugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        EphemeralCoins.BepConfig.EnableArtifact.Value = 2;
+        CostTypeCatalog.GetCostTypeDef(CostTypeIndex.LunarCoin).payCost = (costTypeDef, context) =>
+        {
+            foreach (var master in PlayerCharacterMasterController.instances)
+            {
+                master.networkUser.DeductLunarCoins((uint)context.cost);
+                MultiShopCardUtils.OnNonMoneyPurchase(context);
+            }
+        };
+
+        // Catch up late joiners
+        On.RoR2.Run.OnUserAdded += (orig, self, user) =>
+        {
+            uint max = EphemeralCoins.EphemeralCoins.instance.coinCounts.Max(x => x.ephemeralCoinCount);
+            EphemeralCoins.EphemeralCoins.instance.giveCoinsToUser(user, max);
+        };
+    }
+}
---
+++ LunarCoinSharedPool/README.md
@@ -0,0 +1,5 @@
+# Lunar Coin Shared Pool
+
+All players share a luner coin pool that resets each run, but lunar coins drop more frequently.
+
+Based off the [Ephemeral Coins](https://thunderstore.io/package/VarnaScelestus/Ephemeral_Coins/) and [Lunar Coin Share on Pickup](https://thunderstore.io/package/dan8991iel/LunarCoinShareOnPickup/) mods.
